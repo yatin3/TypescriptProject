f3cb81ca3748aaf18a6a716ccd90a98819d2f305
yatin <yatingoyal31@gmail.com>
2023-02-07 15:01:27 +0530

    initial commit

---
+++ routes/todos.js
@@ -0,0 +1,32 @@
+"use strict";
+// import express from 'express';
+Object.defineProperty(exports, "__esModule", { value: true });
+// const router = express.Router();
+const express_1 = require("express");
+let todos = [];
+const router = (0, express_1.Router)();
+router.get('/', (req, res, next) => {
+    res.status(200).json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    res.status(201).json({ message: 'Added Todo', todo: newTodo, todos: todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: todos[todoIndex].id, text: req.body.text };
+        return res.status(200).json({ message: 'Updated todo', todos: todos });
+    }
+    res.status(404).json({ message: 'could not findtodo for this id.' });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    res.status(200).json({ message: 'Deleted todo', todos: todos });
+});
+exports.default = router;
